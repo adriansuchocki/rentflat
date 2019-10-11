63445270f27f09926adf8a421b9b42a4862b423b
adrian.suchocki <adrian.suchocki@britenet.com.pl>
2019-10-11 15:25:46 +0200

    test

--- aura/RF_Flat_Search/RF_Flat_SearchController.js
+++ aura/RF_Flat_Search/RF_Flat_SearchController.js
@@ -2,8 +2,22 @@
  * Created by BRITENET on 10.10.2019.
  */
 ({
+    init: function(component, event, helper) {
+        var flatsJson = sessionStorage.getItem('RF_Flat_Search--flatList');
+        if (!$A.util.isUndefinedOrNull(flatsJson)) {
+            var flats = JSON.parse(flatsJson);
+            component.set('v.cartList', flats);
+            sessionStorage.removeItem('RF_Flat_Search--flatList');
+        }
+    },
+
     handleSearchFlat: function (component, event, helper) {
-        let fieldText = component.get('v.searchText');
+        let fieldText = component.get("v.searchText");
         helper.getResultList(component, fieldText);
+    },
+
+    handleShowCart: function (component, event, helper) {
+        let cartStatus = component.get("v.cart");
+        component.set("v.cart", !cartStatus);
     }
 })
