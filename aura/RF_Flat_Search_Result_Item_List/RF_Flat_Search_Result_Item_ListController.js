e062f27233ee746c93b0488a40bedd76491fd13d
adrian.suchocki <adrian.suchocki@britenet.com.pl>
2019-10-14 10:57:05 +0200

    Added redirect

--- aura/RF_Flat_Search_Result_Item_List/RF_Flat_Search_Result_Item_ListController.js
+++ aura/RF_Flat_Search_Result_Item_List/RF_Flat_Search_Result_Item_ListController.js
@@ -2,4 +2,11 @@
  * Created by BRITENET on 14.10.2019.
  */
 ({
+    showFlat: function (component, event, helper) {
+        let id = component.get('v.flat.id');
+        var navEvt = $A.get('e.force:navigateToURL');
+        let link = '/product/' +  id;
+        navEvt.setParams({url: link});
+        navEvt.fire();
+    }
 })
